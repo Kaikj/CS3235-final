1c7e5f5096673e47c6afd528cf55a48584a68795
Kaikj <bluesword246@gmail.com>
2016-03-11 08:58:30 +0800

    Define constants

---
+++ constants.py
@@ -0,0 +1,18 @@
+import notes
+
+SAMPLE_RATE = 44100
+CHANNELS = 1
+NUMBER_OF_SECONDS = 5
+ONE_FREQUENCY = 19000
+ZERO_FREQUENCY = 18000
+BYTE_SIZE = 8
+WIDTH = 1
+WIDTH_BITS = WIDTH * BYTE_SIZE
+
+SYMBOL_RATE = 2000
+SAMPLES_PER_SYMBOL = SAMPLE_RATE / SYMBOL_RATE
+
+# When calculating the wave, it is done about the x-axis,
+# so there is a need to correct for this transformation
+TRANSFORMATION = 2 ** (WIDTH_BITS - 1)
+MAX_AMPLITUDE = TRANSFORMATION - 1
