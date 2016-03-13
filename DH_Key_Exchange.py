1c201a4dfad4214003cd9546487aac7c5c99f7ed
ChengKeJing <purpleplasm@gmail.com>
2016-03-14 02:43:57 +0800

    Very basic DH key exchange

---
+++ DH_Key_Exchange.py
@@ -0,0 +1,22 @@
+import math
+
+sharedG = int(raw_input('Shared G value: '))
+sharedP = int(raw_input('Shared P value: '))
+secretA = int(raw_input('Secret A value: '))
+secretB = int(raw_input('Secret B value: '))
+
+def computeSentValue(secret, g = sharedG, p = sharedP):
+	sharedValue = (g ** secret) % p
+	return sharedValue
+
+def computSharedSecret(secret, sharedValue, p = sharedP): 
+	sharedKey = (sharedValue ** secret) % p
+	return sharedKey
+
+sentValueFromA = computeSentValue(secretA)
+sentValueFromB = computeSentValue(secretB)
+sharedKeyForA = computSharedSecret(secretA, sentValueFromB)
+sharedKeyForB = computSharedSecret(secretB, sentValueFromA)
+
+print(sharedKeyForA)
+print(sharedKeyForB)
