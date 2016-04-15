90b88c61e9c3e2488267844aac5200ea3199b4f3
Diga Widyaprana <digawp@gmail.com>
2016-04-15 16:45:00 +0800

    First iteration of VIP client

--- client/client.py
+++ client/client.py
@@ -1,28 +1,25 @@
 from autobahn.twisted.websocket import WebSocketClientProtocol, \
     WebSocketClientFactory
 
+from handler import Handler
 
-class MyClientProtocol(WebSocketClientProtocol):
-
-    def onConnect(self, response):
-        print("Server connected: {0}".format(response.peer))
+class VipClientProtocol(WebSocketClientProtocol):
 
     def onOpen(self):
         print("WebSocket connection open.")
-
-        def hello():
-            self.sendMessage(u"Hello, world!".encode('utf8'))
-            self.sendMessage(b"\x00\x01\x03\x04", isBinary=True)
-            self.factory.reactor.callLater(1, hello)
-
-        # start sending messages every second ..
-        hello()
 
     def onMessage(self, payload, isBinary):
         if isBinary:
             print("Binary message received: {0} bytes".format(len(payload)))
         else:
             print("Text message received: {0}".format(payload.decode('utf8')))
+            key, value = str(payload).split('=')
+            if key == 'g':
+                Handler.setG(value)
+            elif key == 'p':
+                Handler.setP(value)
+            if Handler.gotBothPrimes():
+                Handler.us_auth(self)
 
     def onClose(self, wasClean, code, reason):
         print("WebSocket connection closed: {0}".format(reason))
@@ -38,7 +35,7 @@ if __name__ == '__main__':
     log.startLogging(sys.stdout)
 
     factory = WebSocketClientFactory(u"ws://127.0.0.1:9000", debug=False)
-    factory.protocol = MyClientProtocol
+    factory.protocol = VipClientProtocol
 
     reactor.connectTCP("127.0.0.1", 9000, factory)
     reactor.run()
