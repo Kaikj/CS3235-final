9251eb487b11861ccae57cbad75d74d62aa5c428
Diga Widyaprana <digawp@gmail.com>
2016-04-15 18:20:49 +0800

    Implement normal client protocol, minor refactoring on VIP client protocol

---
+++ client/client.py
@@ -0,0 +1,16 @@
+import sys
+
+from twisted.python import log
+from twisted.internet import reactor
+from autobahn.twisted.websocket import WebSocketClientFactory
+
+from protocols import ClientProtocol
+
+if __name__ == '__main__':
+    log.startLogging(sys.stdout)
+
+    factory = WebSocketClientFactory(u"ws://127.0.0.1:8080/ws", debug=False)
+    factory.protocol = ClientProtocol
+
+    reactor.connectTCP("127.0.0.1", 8080, factory)
+    reactor.run()
--- client/protocols.py
+++ client/protocols.py
@@ -3,14 +3,12 @@ from autobahn.twisted.websocket import WebSocketClientProtocol
 from handlers import VipHandler
 
 class VipClientProtocol(WebSocketClientProtocol):
-
+    """Protocol for VIP client"""
     def onOpen(self):
         print("WebSocket connection open.")
 
     def onMessage(self, payload, isBinary):
-        if isBinary:
-            print("Binary message received: {0} bytes".format(len(payload)))
-        else:
+        if not isBinary:
             print("Text message received: {0}".format(payload.decode('utf8')))
             key, value = str(payload).split('=')
             if key == 'g':
@@ -23,4 +21,23 @@ class VipClientProtocol(WebSocketClientProtocol):
     def onClose(self, wasClean, code, reason):
         print("WebSocket connection closed: {0}".format(reason))
 
+class ClientProtocol(WebSocketClientProtocol):
+    """Protocol for normal clients"""
+    def onOpen(self):
+        print("WebSocket connection open.")
 
+    def onMessage(self, payload, isBinary):
+        if not isBinary:
+            msg = str(payload)
+            print("Text message received: {0}".format(payload.decode('utf8')))
+            key, value = str(payload).split('=')
+            if key == 'g':
+                VipHandler.setG(value)
+            elif key == 'p':
+                VipHandler.setP(value)
+            if VipHandler.gotBothPrimes():
+                self.sendMessage('turn_ready')
+                VipHandler.us_auth(self)
+
+    def onClose(self, wasClean, code, reason):
+        print("WebSocket connection closed: {0}".format(reason))
